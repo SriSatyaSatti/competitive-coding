497f3862fdcd44b1d94768032f94718b858b29e2
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-06-22 10:57:02 +0530

    Update min cost climbing stairs.java

--- Day-5 DP/min cost climbing stairs.java
+++ Day-5 DP/min cost climbing stairs.java
@@ -7,7 +7,7 @@ public class MyClass {
         for(int i=2;i<=n;i++){
             dp[i]=Math.min(dp[i-1],dp[i-2])+a[i-1];
         }
-        return Math.min(dp[n-1],dp[n-2]);
+        return Math.min(dp[n-1],dp[n]);
     }
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
