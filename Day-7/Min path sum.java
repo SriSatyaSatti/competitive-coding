1fc0c4040396f2d8f0ae147e21434de98d3f0ef9
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-06-26 14:01:15 +0530

    Update Min path sum.java

--- Day-7/Min path sum.java
+++ Day-7/Min path sum.java
@@ -11,15 +11,5 @@ int find(int[][] a,int m,int n){
     static int maximumPath(int n, int a[][])
     {
         // code here
-// if (n==1) 
-//         return k;
-//         if (n==2) 
-//         return (long)k*k%1000000007;
-//         long[] dp=new long[n];
-//         dp[0]=k;dp[1]=(long)k*k%1000000007;
-//         for(int i=2;i<n;i++){
-//             dp[i]=((dp[i-1]+dp[i-2])%1000000007*(k-1)%1000000007)%1000000007;
-//         }
-//         return dp[n-1]%1000000007;
        return find(a,n,n)
     }
